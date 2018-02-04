57d3ff56e61aa32618db467048a452f56cf36109
annaq1 <annaq1@uci.edu>
2018-02-03 18:01:24 -0800

    try

--- app/src/main/java/com/example/annar/joystick/MainActivity.java
+++ app/src/main/java/com/example/annar/joystick/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.annar.joystick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        final TextView text= (TextView)findViewById(R.id.text);
+        final TextView text2=(TextView)findViewById(R.id.text2);
+        Button btn1 = (Button) findViewById(R.id.Left);
+        Button btn2 = (Button) findViewById(R.id.Right);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                text.setText("SAF");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                text2.setText("whats good in the hood?");
+            }
+        });
+    }}
+
+
